29a81e1f0b19160644dd738c519a235656c75412
Lauren McCoy <lmccoy8911@gmail.com>
2019-07-20 15:49:29 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase.rb
+++ db/migrate/006_add_catchphrase.rb
@@ -1,7 +1,7 @@
 class AddCatchphrase < ActiveRecord::Migration[5.2]
   
   def change 
-    add_column :character, :catchphrase, :string 
+    add_column :characters, :catchphrase, :string 
   end 
   
   
