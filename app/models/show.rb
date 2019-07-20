763af7310526df9a409955e56d07bcf2d0423f17
Lauren McCoy <lmccoy8911@gmail.com>
2019-07-20 16:03:13 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,8 +2,8 @@ class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
   
-  def actors_list 
-    Actor.full_name
-  end 
+  # def actors_list 
+  #   Actor.full_name
+  # end 
   
 end
